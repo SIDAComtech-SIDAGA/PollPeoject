dcaa4e2049c1eb011f772040d6fedb9e7d37e066
sidaga kihongo <sidagawazirikihongo@gmail.com>
2024-06-16 12:22:35 +0300

    I am unable to create template file with html file in it, so when a server sun it should redirect it to the landing client page

--- pollproject/polls/views.py
+++ pollproject/polls/views.py
@@ -4,4 +4,4 @@ from .models import Choice, Question
 # Get Question and Display them
 
 def index(request):
-    return render(request='polls/index.html')
+    return render(request, 'polls/index.html')
